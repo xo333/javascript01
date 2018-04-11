a20d1db44e6de10b8cf7aaada440b4ebca847d50
jessica balling <jessica.balling@student.uibk.ac.at>
2018-04-11 17:25:40 +0200

    Erste Änderungen

---
+++ main.js
@@ -0,0 +1,30 @@
+// alert("Hello World")
+
+let nachricht = "Das wird spannend"
+// alert (nachricht);
+nachricht = "neuer Text";
+// alert (nachricht);
+console.log (nachricht);
+const linkColor ="#ff0000"
+let eine_variable = "x"
+console.log("Variable: eine_variable", eine_variable)
+let highScore = 58621;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log (meineNachricht);
+meineNachricht = 'test2';
+console.log (meineNachricht);
+meineNachricht = `Hiermit 
+koennen aufwendige 
+mehrzeilige Sachen 
+verkettet werden
+Nur damit`;
+console.log (meineNachricht);
+
+let fullName = 'jeffrey "The Dude" Lebowsky';
+console.log(fullName)
+
+let isOver18 = true;
+console.log ("Über18?", isOver18)
+let age = 19;
+console.log("age über 18", age>18);
